3f309bcce149cb4916a8c7629367a31c99cf12ca
Samuel <heroamogs@gmail.com>
2022-03-09 00:11:05 +0100

    Dynamic password update

--- firstProject/src/firstProject/locators.java
+++ firstProject/src/firstProject/locators.java
@@ -40,9 +40,13 @@ public class locators {
 		
 //		Thread.sleep(2000);
 //		Logging in with correct password:
+		
+//		Dynamically calling the getPassword method
+		String password = getPassword(driver);
+		String name = "Samuel";
 		driver.findElement(By.className("go-to-login-btn")).click();
-		driver.findElement(By.id("inputUsername")).sendKeys("Samuel");
-		driver.findElement(By.name("inputPassword")).sendKeys("rahulshettyacademy");
+		driver.findElement(By.id("inputUsername")).sendKeys(name);
+		driver.findElement(By.name("inputPassword")).sendKeys(password);
 		Thread.sleep(2000);
 		driver.findElement(By.className("signInBtn")).click();
 		
@@ -50,9 +54,36 @@ public class locators {
 		Thread.sleep(2000);
 		System.out.println(driver.findElement(By.cssSelector("body p")).getText());
 		Assert.assertEquals(driver.findElement(By.cssSelector("body p")).getText(), "You are successfully logged in.");
+		
+//		Getting greeting after logged in
+		Assert.assertEquals(driver.findElement(By.tagName("h2")).getText(), "Hello "+name+",");
 		driver.findElement(By.className("logout-btn")).click();
 		driver.close();
 
+	}
+	
+	public static String getPassword(WebDriver driver) throws InterruptedException
+	
+	{
+		driver.get("https://rahulshettyacademy.com/locatorspractice/");
+		driver.findElement(By.linkText("Forgot your password?")).click();
+		Thread.sleep(2000);
+		driver.findElement(By.cssSelector("button[class='reset-pwd-btn']")).click();
+		
+//		Creating a variable for the password text
+		String passwordText = driver.findElement(By.cssSelector("form p")).getText();
+		
+//		Splitting the text to get just the password text (Please use temporary password 'rahulshettyacademy' to Login.)
+//		First, split from the single quote
+//		Note: After split, it will create an array of 2 index (0 and 1). 1st section of the text is 0 index while the other is 1 index.
+//		E.g:
+//		0th index => Please use temporary password '
+//		1st index => rahulshettyacademy' to Login.
+		String[] passwordArray = passwordText.split("'");
+		String password = passwordArray[1].split("'")[0];
+		return password;
+		
+		
 	}
 
 }
